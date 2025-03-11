244ca813972ae47588befafbcfda28b34397f745
UltraUberTurk <morpeh2005ka@gmail.com>
2025-03-12 01:17:59 +0300

    НАГКОДИЛ БЛЯТЬ

--- 1_task/minimization.py
+++ 1_task/minimization.py
@@ -1,15 +1,17 @@
 from mealy_automata import MealyAutomata
 from collections import defaultdict
+from moor_automata import MoorAutomata
+from transformation import mealy_to_moor
 
 def minimization(automata: MealyAutomata):
     reactions = {}
-    # Собираем уникальные реакции
     ind = 1
     table = automata.table
     old_states_to_new = {}
     for s, i in table.items():
-        reaction = tuple(i[a][1] for a in automata.alphabet) # Используем tuple вместо list
+        reaction = tuple(i[a][1] for a in automata.alphabet)
         if reaction not in reactions:
+            # print(s, reaction)
             reactions[reaction] = f"A{ind}"
             ind+=1
     for s, i in table.items():
@@ -30,6 +32,7 @@ def minimization(automata: MealyAutomata):
         for s, i in table1.items():
             reaction1 = tuple(j for _,j in i.items())
             if reaction1 not in reactions1:
+                # print(s, reaction1)
                 reactions1[reaction1] = f"{chr(ind2)}{ind}"
                 ind+=1
         for s, i in table1.items():
@@ -67,24 +70,38 @@ def minimization(automata: MealyAutomata):
 
 # Таблица переходов и реакций для автомата Мили
 table = {
-    "1": {"a": ["7", "0"], "b": ["6", "1"], "c": ["1", "0"]},
-    "2": {"a": ["7", "0"], "b": ["2", "1"], "c": ["1", "0"]},
-    "3": {"a": ["8", "0"], "b": ["6", "1"], "c": ["3", "0"]},
-    "4": {"a": ["8", "0"], "b": ["2", "1"], "c": ["3", "0"]},
-    "5": {"a": ["7", "0"], "b": ["4", "1"], "c": ["3", "0"]},
-    "6": {"a": ["5", "0"], "b": ["1", "1"], "c": ["1", "0"]},
-    "7": {"a": ["1", "1"], "b": ["8", "0"], "c": ["3", "0"]},
-    "8": {"a": ["3", "1"], "b": ["8", "0"], "c": ["1", "0"]}
+    "1": {"a": ["4", "x"], "b": ["7", "y"]},
+    "2": {"a": ["3", "y"], "b": ["4", "y"]},
+    "3": {"a": ["7", "x"], "b": ["9", "x"]},
+    "4": {"a": ["5", "x"], "b": ["9", "y"]},
+    "5": {"a": ["8", "x"], "b": ["7", "y"]},
+    "6": {"a": ["7", "x"], "b": ["9", "y"]},
+    "7": {"a": ["2", "y"], "b": ["8", "x"]},
+    "8": {"a": ["5", "x"], "b": ["9", "y"]},
+    "9": {"a": ["2", "y"], "b": ["5", "x"]}
 }
+
 
 # Список состояний
 states = list(table.keys())
 
 # Начальное состояние
-initial_state = "1"
+initial_state = "8"
 
 # Алфавит входных символов
-alphabet = ["a", "b", "c"]
+alphabet = ["a", "b"]
 
 automata = MealyAutomata(states, initial_state, alphabet, table)
-minimization(automata)
+print("Результат работы над строчкой 'abba' автомата Мили "+"".join([automata.step(i) for i in "abba"]))
+
+automata.table = minimization(automata)
+automata.states = automata.table.keys()
+automata.state = "8"
+print("Результат работы над строчкой 'abba' нормализованного автомата Мили "+"".join([automata.step(i) for i in "abba"]))
+
+res = ""
+moor = mealy_to_moor(automata)
+for i in "abba":
+    moor.step(i)
+    res += moor.get_reaction()
+print(f"Результат работы над строчкой 'abba' автомата Мура {res}")
--- 1_task/transformation.py
+++ 1_task/transformation.py
@@ -5,14 +5,12 @@ from mealy_automata import MealyAutomata
 
 def mealy_to_moor(automata: MealyAutomata) -> MoorAutomata:
     """Transforms a Mealy automaton into a Moore automaton."""
-    # Создаем соответствие между состояниями Mealy и Moore
     state_reactions = defaultdict(set)
     for state in automata.states:
         for symbol in automata.alphabet:
             next_state, reaction = automata.table[state][symbol]
             state_reactions[next_state].add(reaction)
     
-    # Создание новых состояний и реакций
     new_states = {}
     reaction_table = {}
     old_to_new_state = {}
@@ -29,10 +27,9 @@ def mealy_to_moor(automata: MealyAutomata) -> MoorAutomata:
             old_to_new_state[state] = new_state
             state_counter += 1
     
-    # Заполняем таблицу переходов
     transition_table = defaultdict(dict)
     for new_state, old_state in new_states.items():
-        print(1, new_state, old_state)
+        # print(1, new_state, old_state)
         for symbol in automata.alphabet:
             next_old_state, reaction = automata.table[new_state[0]][symbol]
             transition_table[old_state][symbol] = new_states[(next_old_state, reaction)]
